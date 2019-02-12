6dffcfd98697f4702602e11de10126fd6bdd78a8
liuxuechen <liuxuechen01@gmail.com>
2019-02-13 00:44:36 +0800

    problem set 2

---
+++ com5940/problem set 2/mysite/flask_app.py
@@ -0,0 +1,31 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+
+from flask import Flask, render_template, json, request
+from flask import Markup
+import requests
+
+app = Flask(__name__)
+app.config["DEBUG"] = False
+
+@app.route("/")
+def main():
+    user = {"name":"Sharon"}
+    return render_template("index.html",user=user,title="Home Page")
+@app.route("/templates/about.html")
+def about():
+    user = {"name":"Sharon"}
+    return render_template("about.html",user=user,title="About Page")
+@app.route("/templates/products.html")
+def products():
+    user = {"name":"Sharon"}
+    return render_template("products.html",user=user,title="Products Page")
+@app.route("/templates/store.html")
+def store():
+    user = {"name":"Sharon"}
+    return render_template("store.html",user=user,title="Store Page")
+@app.route("/templates/index.html")
+def index():
+    user = {"name":"Sharon"}
+    return render_template("index.html",user=user,title="index Page")
